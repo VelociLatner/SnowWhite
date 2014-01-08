7992f157f39d87148651b20e3fb7b44b8bc281cc
Bryan Latner <blatner@picsauditing.com>
2014-01-08 11:40:59 -0800

    Resolve dependency issues.

--- project/Build.scala
+++ project/Build.scala
@@ -6,13 +6,18 @@ object ApplicationBuild extends Build {
   val appName = "Snow White -- PICS Security Service"
   val appVersion = "0.1"
 
-  lazy val root = Project(id = "root", base = file("."))
-
   val appDependencies = Seq(
     "com.typesafe.slick" % "slick_2.10" % "1.0.1",
     "mysql" % "mysql-connector-java" % "5.1.27",
-    "org.scalatest" % "scalatest_2.10" % "2.0" % "it,test",
-    "org.mockito" % "mockito-all" % "1.9.5" % "it,test"
+    "org.scalatest" % "scalatest_2.10" % "2.0" % "test",
+    "org.mockito" % "mockito-all" % "1.9.5" % "test",
+    "org.apache.shiro" % "shiro-core" % "1.2.2"
   )
 
+  lazy val root = Project(
+    id = "root",
+    base = file(".")
+  ) settings (
+    libraryDependencies ++= appDependencies
+  )
 }
